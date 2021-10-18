8ba6fce8a7bdb77e0138e301ac11ffe9ce3f3734
Fredrik Bendiksen <fredrikben@uia.no>
2021-10-18 13:41:08 +0200

    Created Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace smartmat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
