ebbe534cc3974cac911e96532bb02c33a823a78d
Fredrik Bendiksen <fredrikben@uia.no>
2021-10-20 10:23:42 +0200

    Enabled Users, created ApplicationUser and autogenerated code for user management.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace smartmat.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Placeholder for relationship to recipes
+    }
+}
