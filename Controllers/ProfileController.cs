cf55dd0944f5932028752f101a69697837a22135
Marah Zidan <marahz@uia.no>
2021-10-29 12:10:51 +0200

    Added Profile view, css, controller

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using smartmat.Data;
+
+namespace smartmat.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+            public ProfileController(ILogger<HomeController> logger)
+            {
+                _logger = logger;
+            }
+
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        
+    }
+}
