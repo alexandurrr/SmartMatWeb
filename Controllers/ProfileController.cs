8925d1359b811b79f9efb182eb2927c459a12682
Marah Zidan <marahz@uia.no>
2021-10-30 23:29:02 +0200

    Added view, css, controller to profile

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using smartmat.Data;
+
+namespace smartmat.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
