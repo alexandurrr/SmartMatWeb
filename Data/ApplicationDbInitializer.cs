f9139b98cdca9cf268a7fd2f06127f976f90ee5e
Alexander Gyatso <alexandeg@uia.no>
2021-11-03 20:51:37 +0100

    Connected test data to Users, without connection recipe search does not work

--- Data/ApplicationDbInitializer.cs
+++ Data/ApplicationDbInitializer.cs
@@ -14,22 +14,8 @@ namespace smartmat.Data
         {
             db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
-            //testdata
-            var recipes = new[]
-            {
-                new Recipe("Eggerøre", "intro1", "1 Egg, 2dl Melk, 400g Ost, 1stk Paprika, Salt, Pepper",
-                    "Bland alt i en bolle og hiv det i panna.", "20g protein, 50g karbohydrat, 10g fett", "https://images.matprat.no/nr32p8psxu-jumbotron/large"),
-                new Recipe("Grønnsakssuppe", "intro2", "1stk Brokkoli, 1/2 Blomkål, 4 Potet, 1 Sellerirot, 2 Gulrot, Salt, Pepper",
-                    "Kutt opp grønnsakene og kok i 20min. Smak til med salt og pepper.", "10g protein, 50g karbohydrat, 5g fett", "https://images.matprat.no/fmg25b4e53-jumbotron/large"),
-                new Recipe("Stekt laks med grønnsaker", "intro3", "400g Laks, 1 Brokkoli, 5 Potet, 3 Gulrot, 100g Smør, Salt, Pepper",
-                    "Kutt opp grønnsakene og damp i 10min. Stek laksen i panne i 10min. Smak til med salt og pepper.",
-                    "30g protein, 50g karbohydrat, 10g fett", "https://images.matprat.no/ur4kxnnmfa-jumbotron/large")
-                
-            };
-
-            db.Recipes.AddRange(recipes);
-            db.SaveChanges();
             
+
             var user = new ApplicationUser
             {
                 UserName = "user@uia.no", 
@@ -42,9 +28,43 @@ namespace smartmat.Data
             };
             um.CreateAsync(user, "Password1.").Wait();
             db.SaveChanges();
-
+            
+            // Test data, while also adding a USER relation
             foreach (var u in db.Users.Include(b => b.Recipes) )
             {
+                u.Recipes.Add(new Recipe
+                {
+                    Title = "Eggerøre",
+                    Introduction = "intro1",
+                    Ingredients = "1 Egg, 2dl Melk, 400g Ost, 1stk Paprika, Salt, Pepper",
+                    Instructions = "Kutt opp grønnsakene og damp i 10min. Stek laksen i panne i 10min. Smak til med salt og pepper.",
+                    Nutrients = "20g protein, 50g karbohydrat, 10g fett",
+                    Image = "https://images.matprat.no/6pmvkb4usd-jumbotron/large"
+                    
+                });
+                u.Recipes.Add(new Recipe
+                {
+                    Title = "Grønnsakssuppe",
+                    Introduction = "intro2",
+                    Ingredients = "1stk Brokkoli, 1/2 Blomkål, 4 Potet, 1 Sellerirot, 2 Gulrot, Salt, Pepper",
+                    Instructions = "Kutt opp grønnsakene og kok i 20min. Smak til med salt og pepper.",
+                    Nutrients = "10g protein, 50g karbohydrat, 5g fett",
+                    Image = "https://images.matprat.no/fmg25b4e53-jumbotron/large"
+                    
+                });
+                u.Recipes.Add(new Recipe
+                {
+                    Title = "Stekt laks med grønnsakere",
+                    Introduction = "intro3",
+                    Ingredients = "400g Laks, 1 Brokkoli, 5 Potet, 3 Gulrot, 100g Smør, Salt, Pepper",
+                    Instructions = "Kutt opp grønnsakene og damp i 10min. Stek laksen i panne i 10min. Smak til med salt og pepper.",
+                    Nutrients = "30g protein, 50g karbohydrat, 10g fett",
+                    Image = "https://images.matprat.no/hs2frzl2jk-jumbotron/large"
+                    
+                });
+
+                /*
+                 // For test values with just numbers
                 for (int i = 1; i < 3; i++)
                 {
                     u.Recipes.Add(new Recipe
@@ -56,7 +76,7 @@ namespace smartmat.Data
                         Nutrients =  $"Næringsstoffer {i}",
                         Image = $"{i}"
                     });
-                }
+                }*/
             }
             
             for (int i = 1; i < 3; i++)
