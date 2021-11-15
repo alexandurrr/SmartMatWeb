f70e2cc1d12691081f41563880d1687c1d79c698
Alexander Gyatso <alexandeg@uia.no>
2021-11-15 17:12:55 +0100

    Added non-file columns from recipe, and Image IFormFile

--- Models/RecipeUserViewModel.cs
+++ Models/RecipeUserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using smartmat.Data;
 
 namespace smartmat.Models
@@ -8,5 +9,25 @@ namespace smartmat.Models
         public ICollection<Recipe> Recipes { get; set; }
         public ICollection<ApplicationUser> Users { get; set; }
         public ICollection<Review> Reviews { get; set; }
+        
+        // Bool to check if post is success
+        public bool IsSuccess { get; set; }
+
+        // IFormFile used to add image to wwwroot/image
+        public IFormFile Image { get; set; }
+    
+        // Recipes columns
+        public string Title { get; set; }
+   
+        public string Introduction { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Instructions { get; set; }
+
+        public string Nutrients { get; set; }
+
+        public string Visibility { get; set; }
+
     }
 }
