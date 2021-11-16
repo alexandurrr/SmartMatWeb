5edcdd3e64e628427b050e553fd03d38c68a0681
Alexander Gyatso <alexandeg@uia.no>
2021-11-16 22:13:54 +0100

    Removed extra fields in RecipeUserVM, added to new VM

--- Models/RecipeUserViewModel.cs
+++ Models/RecipeUserViewModel.cs
@@ -11,31 +11,5 @@ namespace smartmat.Models
         public ICollection<ApplicationUser> Users { get; set; }
         public ICollection<Review> Reviews { get; set; }
         
-        // Bool to check if post is success
-        public bool IsSuccess { get; set; }
-        
-        // IFormFile used to add image to wwwroot/recipeimages
-        [Required]
-        public IFormFile Image { get; set; }
-    
-        // Recipes columns
-        [Required]
-        public string Title { get; set; }
-   
-        [Required]
-        public string Introduction { get; set; }
-
-        [Required]
-        public string Ingredients { get; set; }
-
-        [Required]
-        public string Instructions { get; set; }
-
-        [Required]
-        public string Nutrients { get; set; }
-
-        [Required]
-        public string Visibility { get; set; }
-
     }
 }
