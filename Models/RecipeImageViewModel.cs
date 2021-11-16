713cb9de8b473206433e6628740ef66948b962a0
Alexander Gyatso <alexandeg@uia.no>
2021-11-16 22:15:37 +0100

    Added new VM to divide up existing VMs

---
+++ Models/RecipeImageViewModel.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using smartmat.Data;
+
+namespace smartmat.Models
+{
+    public class RecipeImageViewModel
+    {
+        // Bool to check if post is success
+        public bool IsSuccess { get; set; }
+        
+        // IFormFile used to add image to wwwroot/recipeimages
+        [Required]
+        public IFormFile Image { get; set; }
+    
+        // Recipes columns
+        [Required]
+        public string Title { get; set; }
+   
+        [Required]
+        public string Introduction { get; set; }
+
+        [Required]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        [Required]
+        public string Nutrients { get; set; }
+
+        [Required]
+        public string Visibility { get; set; }
+    }
+}
