33760d26077efc0acd7215c4fba4686b5bd461c1
Alexander Gyatso <alexandeg@uia.no>
2021-11-16 14:53:12 +0100

    Added new fields to VM, added required to attributes

--- Models/RecipeUserViewModel.cs
+++ Models/RecipeUserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 using smartmat.Data;
 
@@ -12,21 +13,28 @@ namespace smartmat.Models
         
         // Bool to check if post is success
         public bool IsSuccess { get; set; }
-
-        // IFormFile used to add image to wwwroot/image
+        
+        // IFormFile used to add image to wwwroot/recipeimages
+        [Required]
         public IFormFile Image { get; set; }
     
         // Recipes columns
+        [Required]
         public string Title { get; set; }
    
+        [Required]
         public string Introduction { get; set; }
 
+        [Required]
         public string Ingredients { get; set; }
 
+        [Required]
         public string Instructions { get; set; }
 
+        [Required]
         public string Nutrients { get; set; }
 
+        [Required]
         public string Visibility { get; set; }
 
     }
