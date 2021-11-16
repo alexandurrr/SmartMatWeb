4e0abfc130d3a7c136b611b669dc2fc2cda11470
John Nicklas Bernhardsen <johnnb17@uia.no>
2021-11-16 13:11:52 +0100

    Modified meals controller adding support for public view, author and review score

--- Controllers/MealsController.cs
+++ Controllers/MealsController.cs
@@ -3,44 +3,98 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using smartmat.Data;
 using smartmat.Models;
+using SQLitePCL;
 
 namespace smartmat.Controllers
 {
     public class MealsController : Controller
     {
-        private readonly ILogger<MealsController> _logger;
-
-        public MealsController(ILogger<MealsController> logger)
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public MealsController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
-            _logger = logger;
+            _db = db;
+            _userManager = userManager;
+        }
+        
+        public async Task<IActionResult> Breakfast()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new RecipeUserViewModel
+            {
+                Users = _userManager.Users.ToList(),
+                Recipes = _db.Recipes
+                    .Where(recipe => recipe.Visibility == "Public" || recipe.ApplicationUser == user)
+                    .OrderByDescending(recipe => recipe.Reviews.Average(r => r.Stars))
+                    .ToList(),
+                Reviews = _db.Reviews.ToList()
+            };
+            return View(vm);
         }
 
-        public IActionResult FastFood()
+        public async Task<IActionResult> FastFood()
         {
-            return View();
-        }        
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new RecipeUserViewModel
+            {
+                Users = _userManager.Users.ToList(),
+                Recipes = _db.Recipes
+                    .Where(recipe => recipe.Visibility == "Public" || recipe.ApplicationUser == user)
+                    .OrderByDescending(recipe => recipe.Reviews.Average(r => r.Stars))
+                    .ToList(),
+                Reviews = _db.Reviews.ToList()
+            };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Lunch()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new RecipeUserViewModel
+            {
+                Users = _userManager.Users.ToList(),
+                Recipes = _db.Recipes
+                    .Where(recipe => recipe.Visibility == "Public" || recipe.ApplicationUser == user)
+                    .OrderByDescending(recipe => recipe.Reviews.Average(r => r.Stars))
+                    .ToList(),
+                Reviews = _db.Reviews.ToList()
+            };
+            return View(vm);
+        }       
         
-        public IActionResult Breakfast()
+        public async Task<IActionResult> Dinner()
         {
-            return View();
-        }        
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new RecipeUserViewModel
+            {
+                Users = _userManager.Users.ToList(),
+                Recipes = _db.Recipes
+                    .Where(recipe => recipe.Visibility == "Public" || recipe.ApplicationUser == user)
+                    .OrderByDescending(recipe => recipe.Reviews.Average(r => r.Stars))
+                    .ToList(),
+                Reviews = _db.Reviews.ToList()
+            };
+            return View(vm);
+        }      
         
-        public IActionResult Lunch()
+        public async Task<IActionResult> Dessert()
         {
-            return View();
-        }        
-        
-        public IActionResult Dinner()
-        {
-            return View();
-        }        
-        
-        public IActionResult Dessert()
-        {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new RecipeUserViewModel
+            {
+                Users = _userManager.Users.ToList(),
+                Recipes = _db.Recipes
+                    .Where(recipe => recipe.Visibility == "Public" || recipe.ApplicationUser == user)
+                    .OrderByDescending(recipe => recipe.Reviews.Average(r => r.Stars))
+                    .ToList(),
+                Reviews = _db.Reviews.ToList()
+            };
+            return View(vm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
